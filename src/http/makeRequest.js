c486befb562de1f9e6604ee310a8cb7de5e08bc9
Sultan <sembinovsultan@gmail.com>
2023-06-10 10:38:06 +0600

    fixed

--- src/http/makeRequest.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const baseURL = '/fly.json'
-
-export default ({ apiUrl = '/', method = 'get', params = {}, data = {}, headers = {} }) =>
-    axios({ baseURL, url: apiUrl, method, params, data, headers });
--- src/http/rest/getFlight.js
+++
@@ -1,6 +0,0 @@
-import makeRequest from "../makeRequest";
-
-export const getFlight = () =>
-    makeRequest({
-        method: 'GET'
-    })
